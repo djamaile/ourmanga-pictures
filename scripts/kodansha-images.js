4ea074f4c4a08339c9595bfc5dd00c3704ec2131
djamaile <rdjamaile@gmail.com>
2023-05-05 16:30:31 +0200

    chore: update pictures
    Signed-off-by: djamaile <rdjamaile@gmail.com>

--- scripts/kodansha-images.js
+++ scripts/kodansha-images.js
@@ -6,17 +6,15 @@ import * as sharp from "sharp"
 async function getImages(url) {
     const response = await fetch(url);
     const mangas = await response.json();
-    mangas.map(async manga => await downloadImage(manga.image, manga.image.split("/").at(-1)));
+    mangas.map(async manga => await downloadImage(manga.image, manga.name.replace(/[^\w\s]/gi, '').replace(/\s+/g, '')));
 }
-
-const transformer = (imageName) => sharp().toFormat("jpg");
 
 const downloadImage = async (url, imageName) => {
     console.info(`fetching image for ${url}`)
     imageName = imageName.split("?")[0];
-    fetch(url)
+    await fetch(url)
 	.then(res =>
-	   res.body.pipe(fs.createWriteStream(path.resolve("kodansha", imageName)))
+	   res.body.pipe(fs.createWriteStream(path.resolve("kodansha", imageName + ".jpg")))
 	)
     .catch(e => console.error(e));
 }
